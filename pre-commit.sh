afaa603187262e4d1cdac706f047d59a302484ec
Sanjit-Prasad <sanjitpd777@gmail.com>
2022-11-11 15:16:00 +0530

    Git Hooks | pre-commit checks

--- pre-commit.sh
+++ pre-commit.sh
@@ -30,14 +30,24 @@ else
   exit 1;
 fi;
 
-
+# Check if this is the initial commit
 if git rev-parse --verify HEAD >/dev/null 2>&1
 then
-        against=HEAD
+    echo "pre-commit: About to create a new commit..."
+    against=HEAD
 else
-        # Initial commit: diff against an empty tree object
-        against=$(git hash-object -t tree /dev/null)
+    echo "pre-commit: About to create the first commit..."
+    against=4b825dc642cb6eb9a060e54bf8d69288fbee4904
+fi
+
+# Use git diff-index to check for whitespace errors
+echo "pre-commit: Testing for whitespace errors..."
+if ! git diff-index --check --cached $against
+then
+    echo "pre-commit: Aborting commit due to whitespace errors"
+    exit 1
+else
+    echo "pre-commit: No whitespace errors :)"
+    exit 0
 fi;
 
-# If there are whitespace errors, print the offending file names and fail.
-exec git diff-index --check --cached $against --
